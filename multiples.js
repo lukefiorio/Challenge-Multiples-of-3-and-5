bb9cb159bc2f7956509c7e0c790c4a9655ab8f16
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-02 18:16:54 -1000

    applied autoformatting

--- multiples.js
+++ multiples.js
@@ -3,16 +3,16 @@
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
  * @return {Number}
  */
-exports.sumOfAMultiple = function( n ) {
+exports.sumOfAMultiple = function (n) {
   var sum = 0;
 
   // do your work here
-  for (var i=1;i<n;i++) {
-    if (i%3 === 0 || i%5 === 0) {
-      sum +=i;
+  for (var i = 1; i < n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
     }
   }
-  
+
   return sum;
 };
 
@@ -26,7 +26,7 @@ function sumMult( n ) {
       sum +=i;
     }
   }
-  
+
   return sum;
 }
 console.log(sumMult(10000));
